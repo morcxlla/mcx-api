854abd7b449b3145c788121b786e24d5476772c6
morcxlla <jorgecividanes2016@gmail.com>
2025-12-11 23:59:36 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MCX-API
+
+Multi-Purpose API. Created with NextJS + Elysia. Tested with HTTPie.
+
+Requirements:
+
+- bun
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <main>
+      <h1>hello world</h1>
+    </main>
+  )
+}
+
+export default Index
